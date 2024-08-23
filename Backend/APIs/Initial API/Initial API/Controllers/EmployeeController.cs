caaf9ce013e64c4d94e59ec00fe8a7f507d8950a
Adonis Paniagua <darkendord6@gmail.com>
2024-08-23 17:54:39 -0400

    Started to add cors policy

---
+++ Backend/APIs/Initial API/Initial API/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Initial_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeController() 
+        { 
+        }
+
+        [HttpGet("GetEmployees/{testValue}")]
+        // public IActionResult GetEmployees
+        public string[] GetEmployees(string testValue)
+        {
+            string[] responseArray = new string[]
+            {
+                "Employee 1",
+                "Employee 2",
+                "Employee 3"
+            };
+            return responseArray;
+        }
+    }
+}
