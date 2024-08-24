1054d0f562ad79230268d02908ca94841ba5a72c
Adonis Paniagua <darkendord6@gmail.com>
2024-08-24 10:07:18 -0400

    Created the first models

---
+++ Backend/APIs/Initial API/Initial API/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Initial_API.Models
+{
+    public partial class Notes
+    {
+        public DateTime CreationDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string Detail { get; set; }
+        public int IdNote { get; set; }
+        public int IdCustomer { get; set; }
+        public int EmployeeNumber { get; set; }
+    }
+}
