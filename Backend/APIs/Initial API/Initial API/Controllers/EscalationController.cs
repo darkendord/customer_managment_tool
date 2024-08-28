7b17001077b091e4fcfc7350b28af0581f9c3e70
Adonis Paniagua <darkendord6@gmail.com>
2024-08-28 01:41:31 -0400

    Added endpoints for Escalations and Notes

---
+++ Backend/APIs/Initial API/Initial API/Controllers/EscalationController.cs
@@ -0,0 +1,91 @@
+﻿using Initial_API.Data;
+using Initial_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Initial_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EscalationController : ControllerBase
+    {
+        DataContextEF _entityFramework;
+        public EscalationController(IConfiguration config)
+        {
+            _entityFramework = new DataContextEF(config);
+        }
+
+        [HttpGet("GetEscalations")]
+        public IEnumerable<EscalationModel> GetEscalations()
+        {
+            IEnumerable<EscalationModel> Escalations = _entityFramework.Escalations.ToList<EscalationModel>();
+            return Escalations;
+        }
+
+        [HttpGet("GetSingleEscalation/{idEscalation}")]
+        public EscalationModel GetSingleEscalation(int idEscalation)
+        {
+            EscalationModel? Escalation = _entityFramework.Escalations.Where(Escalation => Escalation.IdEscalation == idEscalation).FirstOrDefault();
+
+            if (Escalation != null)
+            {
+                return Escalation;
+            }
+            throw new Exception("Failed to Get Escalation");
+        }
+
+        [HttpPut("EditEscalation")]
+        public IActionResult EditEscalation(EscalationModel escalation)
+        {
+            EscalationModel? EscalationOnDb = _entityFramework.Escalations.Where(Escalation => Escalation.IdEscalation == escalation.IdEscalation).FirstOrDefault();
+
+            if (EscalationOnDb != null)
+            {
+                EscalationOnDb.EmployeeNumber = escalation.EmployeeNumber;
+                EscalationOnDb.Notes = escalation.Notes ?? EscalationOnDb.Notes;
+                EscalationOnDb.IdCustomer = escalation.IdCustomer;
+
+                if (_entityFramework.SaveChanges() > 0)
+                {
+                    return Ok(EscalationOnDb);
+                }
+            }
+            throw new Exception("Failed to Update or edit Escalation or was not found");
+        }
+
+        [HttpPost("PostEscalation")]
+        public IActionResult AddEscalation(EscalationModel Escalation)
+        {
+            EscalationModel EscalationToDb = new EscalationModel();
+
+            EscalationToDb.Notes = Escalation.Notes;
+            EscalationToDb.IdCustomer = Escalation.IdCustomer;
+            EscalationToDb.EmployeeNumber = Escalation.EmployeeNumber;
+            EscalationToDb.CreationDate = DateTime.Now;
+            EscalationToDb.Departament = Escalation.Departament;
+
+            _entityFramework.Add(EscalationToDb);
+            if (_entityFramework.SaveChanges() > 0)
+            {
+                return Ok(EscalationToDb);
+            }
+            throw new Exception("Failed to Add Escalation");
+        }
+
+        [HttpDelete("DeleteEscalation/{idEscalation}")]
+        public IActionResult DeleteEscalation(int idEscalation)
+        {
+            EscalationModel? EscalationOnDb = _entityFramework.Escalations.Where(Escalation => Escalation.IdEscalation == idEscalation).FirstOrDefault<EscalationModel>();
+
+            if (EscalationOnDb != null)
+            {
+                _entityFramework.Escalations.Remove(EscalationOnDb);
+                if (_entityFramework.SaveChanges() > 0)
+                {
+                    return Ok(EscalationOnDb);
+                }
+            }
+            throw new Exception("Failed to Update or delete Escalation");
+
+        }
+    }
+}
---
+++ Backend/APIs/Initial API/Initial API/Controllers/NoteController.cs
@@ -0,0 +1,92 @@
+﻿using Initial_API.Data;
+using Initial_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Initial_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NoteController : ControllerBase
+    {
+        DataContextEF _entityFramework;
+        public NoteController(IConfiguration config)
+        {
+            _entityFramework = new DataContextEF(config);
+        }
+
+        [HttpGet("GetNotes")]
+        public IEnumerable<NoteModel> GetNotes()
+        {
+            IEnumerable<NoteModel> Notes = _entityFramework.Notes.ToList<NoteModel>();
+            return Notes;
+        }
+
+        [HttpGet("GetSingleNote/{idNote}")]
+        public NoteModel GetSingleNote(int idNote)
+        {
+            NoteModel? Note = _entityFramework.Notes.Where(Note => Note.IdNote == idNote).FirstOrDefault();
+
+            if (Note != null)
+            {
+                return Note;
+            }
+            throw new Exception("Failed to Get Note");
+        }
+
+        [HttpPut("EditNote")]
+        public IActionResult EditNote(NoteModel note)
+        {
+            NoteModel? NoteOnDb = _entityFramework.Notes.Where(Note => Note.IdNote == note.IdNote).FirstOrDefault();
+
+            if (NoteOnDb != null)
+            {
+                NoteOnDb.CreatedBy = note.CreatedBy ?? NoteOnDb.CreatedBy;
+                NoteOnDb.Detail = note.Detail ?? NoteOnDb.Detail;
+                NoteOnDb.IdCustomer = NoteOnDb.IdCustomer;
+                NoteOnDb.EmployeeNumber = NoteOnDb.EmployeeNumber;
+
+                if (_entityFramework.SaveChanges() > 0)
+                {
+                    return Ok(NoteOnDb);
+                }
+            }
+            throw new Exception("Failed to Update or edit Note or was not found");
+        }
+
+        [HttpPost("PostNote")]
+        public IActionResult AddNote(NoteModel note)
+        {
+            NoteModel NoteToDb = new NoteModel();
+
+            NoteToDb.CreatedBy = note.CreatedBy;
+            NoteToDb.Detail = note.Detail;
+            NoteToDb.IdCustomer = note.IdCustomer;
+            NoteToDb.EmployeeNumber = note.EmployeeNumber;
+            NoteToDb.CreationDate = DateTime.Now;
+
+            _entityFramework.Add(NoteToDb);
+            if (_entityFramework.SaveChanges() > 0)
+            {
+                return Ok(NoteToDb);
+            }
+            throw new Exception("Failed to Add Note");
+        }
+
+        [HttpDelete("DeleteNote/{idNote}")]
+        public IActionResult DeleteNote(int idNote)
+        {
+            NoteModel? NoteOnDb = _entityFramework.Notes.Where(Note => Note.IdNote == idNote).FirstOrDefault<NoteModel>();
+
+            if (NoteOnDb != null)
+            {
+                _entityFramework.Notes.Remove(NoteOnDb);
+                if (_entityFramework.SaveChanges() > 0)
+                {
+                    return Ok(NoteOnDb);
+                }
+            }
+            throw new Exception("Failed to Update or delete Note");
+
+        }
+    }
+}
